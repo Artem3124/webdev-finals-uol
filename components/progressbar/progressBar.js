4ad19d6737576a7115b0d1cf621c5192c6ae58a3
Artem3124 <mr.zhukovsky02@gmail.com>
2024-08-13 20:57:55 +0200

    skills wip

---
+++ components/progressbar/progressBar.js
@@ -0,0 +1,38 @@
+const progressBar = (
+  params = { element, initialValue, maxValue, speed, isEnd },
+) => {
+  const {
+    element = element ?? console.error("element is required"),
+    initialValue = 10,
+    maxValue = 100,
+    speed = 10,
+    isEnd = false,
+  } = params;
+
+  let progress = initialValue;
+  let endReached = isEnd;
+  let elementRef = element;
+
+  return {
+    ...params,
+    move: () => {
+      if (endReached) {
+        return;
+      }
+      // endReached = true;
+      var interval = setInterval(() => {
+        if (progress >= maxValue) {
+          clearInterval(interval);
+          endReached = true;
+        } else {
+          progress++;
+          console.log("progress", progress);
+          elementRef.style.width = progress + "%";
+          elementRef.innerHTML = progress + "%";
+        }
+      }, speed);
+    },
+  };
+};
+
+export { progressBar };
