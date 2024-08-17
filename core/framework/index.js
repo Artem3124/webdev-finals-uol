e472c2045072b8521cdaf0a2e607d92570b5b197
Artem3124 <mr.zhukovsky02@gmail.com>
2024-08-17 19:47:37 +0200

    initated reusability structure

--- core/framework/index.js
+++ core/framework/index.js
@@ -3,7 +3,24 @@ const importHtml = (props = { path, elementId, document }) => {
     fetch(props.path)
       .then((response) => response.text())
       .then((data) => {
-        props.document.getElementById(props.elementId).innerHTML = data;
+        const element = props.document.getElementById(props.elementId);
+        element.innerHTML = data;
+
+        const internalScripts = element.querySelectorAll("script:not([src])");
+        internalScripts.forEach((script) => {
+          const newScript = props.document.createElement("script");
+          newScript.textContent = script.textContent;
+          newScript.type = "module";
+          console.log("script", newScript);
+          document.body.appendChild(newScript);
+        });
+
+        const externalScripts = element.querySelectorAll("script[src]");
+        externalScripts.forEach((script) => {
+          const newScript = props.document.createElement("script");
+          newScript.src = script.src;
+          document.body.appendChild(newScript);
+        });
       })
       .catch((error) => console.error("Error loading HTML:", error));
   });
